ab6db13e43c9d910716ac2942c47baec02032857
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-06-09 23:01:49 -0400

    Día 3 terminado

--- 03_Day/03_day_starter/scripts/main.js
+++ 03_Day/03_day_starter/scripts/main.js
@@ -3,12 +3,37 @@ let firstName = "Enrique",
   lastName = "Gimenez",
   country = "Paraguay",
   city = "San Lorenzo",
-  age = 22,
+  birthDate = 1997,
+  ahora = new Date(),
+  age = () => {
+    if (ahora.getMonth() <= 10) {
+      return ahora.getFullYear() - (birthDate + 1);
+    } else {
+      return ahora.getFullYear();
+    }
+  },
   isMarried = false,
-  year = 2020;
+  year = ahora.getFullYear(),
+  estadoCivil = () => {
+    if (isMarried === false) {
+      return "soltero";
+    } else {
+      return "casado";
+    }
+  },
+  independenciaPY = 1811,
+  agePY = ahora.getFullYear() - independenciaPY;
 
-console.log(typeof "10" == 10);
-console.log(parseInt("9.8" == 10));
+let ficha = `Mi nombre es ${firstName} ${lastName}, tengo ${age()} años, vivo en ${city}-${country}. Soy ${estadoCivil()}, y estamos en el año ${year}.`;
 
-typeof "20" === 20;
-typeof "35" === 35;
+let enPantallaFicha = document.getElementById("ficha");
+enPantallaFicha.innerHTML = ficha;
+
+//age  phrase
+
+let ageDiff = agePY - age();
+
+let yearsOlderPhrase = `Mi país es ${ageDiff} años más viejo que yo.`;
+
+let enPantallaPais = document.getElementById("pais");
+enPantallaPais.innerHTML = yearsOlderPhrase;
