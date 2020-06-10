e24b8fcc22a0e2a011ef603c2b00177daaa0475b
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-06-09 21:47:20 -0400

    day 3 first commit

--- 03_Day/03_day_starter/scripts/main.js
+++ 03_Day/03_day_starter/scripts/main.js
@@ -1,1 +1,14 @@
 // this is your main.js script
+let firstName = "Enrique",
+  lastName = "Gimenez",
+  country = "Paraguay",
+  city = "San Lorenzo",
+  age = 22,
+  isMarried = false,
+  year = 2020;
+
+console.log(typeof "10" == 10);
+console.log(parseInt("9.8" == 10));
+
+typeof "20" === 20;
+typeof "35" === 35;
