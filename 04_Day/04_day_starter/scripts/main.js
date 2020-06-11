b5f9c789412382b4478ecd59f94f2a5bc107f00f
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-06-10 22:30:33 -0400

    Día 4 terminado

--- 04_Day/04_day_starter/scripts/main.js
+++ 04_Day/04_day_starter/scripts/main.js
@@ -1,3 +1,25 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+let allowedToDrive = () => {
+  if ((age = 30)) {
+    console.log("You are old enough to drive");
+  } else {
+    console.log("You are left with 3 years to drive");
+  }
+};
+
+let studentsGrade = (nota) => {
+  if (nota <= 100 && nota >= 80) {
+    return "A";
+  } else if (nota >= 70 && nota <= 89) {
+    return "B";
+  } else if (nota >= 60 && nota <= 69) {
+    return "C";
+  } else if (nota >= 50 && nota <= 59) {
+    return "D";
+  } else if (nota >= 0 && nota <= 49) {
+    return "F";
+  }
+};
+
+console.log(studentsGrade(24));
