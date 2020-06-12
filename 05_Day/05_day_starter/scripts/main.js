7a36dbe672d504a2e9abd57c1d570298a218f9c6
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-06-11 22:14:19 -0400

    dia 5!

--- 05_Day/05_day_starter/scripts/main.js
+++ 05_Day/05_day_starter/scripts/main.js
@@ -1,3 +1,52 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+const sudamerica = [
+  'Paraguay',
+  'Brasil',
+  'Argentina',
+  'Uruguay',
+  'Chile',
+  'Venezuela',
+  'Brasil',
+  'Bolivia',
+  'Ecuador',
+  'Perú',
+  'Colombia',
+];
+
+let totalSud = sudamerica.length;
+
+console.log(totalSud);
+
+let orderedRegion = sudamerica.sort();
+
+console.log('Paises ordenados', orderedRegion);
+
+const clubesDePrimera = [
+  'Olimpia',
+  'Cerro Porteño',
+  'Nacional',
+  'Libertad',
+  'Guaraní',
+  'Sol de América',
+  'Sportivo Luqueño',
+  'San Lorenzo',
+  'River Plate',
+  'Guaireña FC',
+  'Gral. Díaz',
+  '12 de octubre',
+];
+
+let rankingDeClubes = clubesDePrimera.sort();
+
+console.log(clubesDePrimera);
+
+const clubesDeIntermedia = [
+  'Rubio Ñu',
+  'Capiatá',
+  'Caaguazú',
+  '3 de febrero',
+  '2 de mayo',
+  'Atyrá',
+  'Cristóbal Colón',
+  'Trinidense',
+  'Tacuary',
+];
